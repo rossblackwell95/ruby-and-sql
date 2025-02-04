8a53ec6cb8f7dae18229c0e78057621c82cd89a3
Ross Blackwell <ross.blackwell@kellogg.northwestern.edu>
2025-02-04 22:13:11 +0000

    Lab 2 Done

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -14,8 +14,6 @@ Company.destroy_all
 # 1a. check out the schema file
 # 1b. check out the model file
 
-
-
 # 2. insert new rows in companies table
 
 new_company = Company.new
--- labs/2-models.rb
+++ labs/2-models.rb
@@ -21,12 +21,37 @@ Salesperson.destroy_all
 
 # 2. insert 1-2 rows in salespeople table.
 
+new_salesperson = Salesperson.new
+new_salesperson["first_name"] = "Ross"
+new_salesperson["last_name"] = "Blackwell"
+new_salesperson["email"] = "rossblackwell95@gmail.com"
+new_salesperson["phone"] = "615-715-0938"
+new_salesperson.save
+
+# puts new_salesperson.inspect
+
+new_salesperson = Salesperson.new
+new_salesperson["first_name"] = "Conor"
+new_salesperson["last_name"] = "McGanon"
+new_salesperson["email"] = "conorm@gmail.com"
+new_salesperson["phone"] = "312-928-2827"
+new_salesperson.save
+
+# puts new_salesperson.inspect
+
 # 3. write code to display how many salespeople rows are in the database
+
+# puts "There are #{Salesperson.all.count} salespeople"
 
 # ---------------------------------
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
+
+# ross = Salesperson.find_by({ "first_name" => "Ross" })
+# ross["first_name"] = "Franklin"
+# puts ross.inspect
+# ross.save
 
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
@@ -35,3 +60,16 @@ Salesperson.destroy_all
 # Salespeople: 2
 # Ben Block
 # Brian Eng
+
+puts "Salespeople: #{Salesperson.all.count}"
+
+salespeople = Salesperson.all
+
+for salesperson in salespeople
+    first_name = salesperson["first_name"]
+    last_name = salesperson["last_name"]
+    full_name = "#{first_name} #{last_name}"
+    puts full_name
+end
+
+
